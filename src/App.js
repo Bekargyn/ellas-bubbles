5a3ff856063c8903b78926e688080017410fd42e
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-04-28 17:07:44 -0500

    set up the scss and components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles/scss/style.scss";
 
 function App() {
   return <div className="App">Hello World</div>;
