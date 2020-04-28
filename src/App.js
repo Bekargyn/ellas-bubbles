c83aad20f26f7cf154d4875152968c36ab67f29c
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-04-28 18:41:05 -0500

    install sass and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Main } from "./components/main.js";
 import "./styles/scss/style.scss";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
