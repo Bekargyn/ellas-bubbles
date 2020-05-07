4ebb9d4912e2573e7fc3e65123dc15114f00b702
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-05-07 16:18:17 -0500

    removed finance.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Main } from "./components/main.js";
-import { Finance } from "./components/financing.js";
 import "./styles/scss/style.scss";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/finance" component={Finance} />
         </Switch>
       </Router>
     </div>
